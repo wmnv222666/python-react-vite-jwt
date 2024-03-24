09c4d03ced56900098cb9a06705cb7f3b29ccc39
wei <wmnv222666@163.com>
2024-03-24 15:09:04 -0400

    test13

--- backend/blog_api/views.py
+++ backend/blog_api/views.py
@@ -1,14 +1,47 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import (
+    SAFE_METHODS,
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
+    BasePermission,
+    IsAdminUser,
+    DjangoModelPermissions,
+    AllowAny,
+)
+
+class PostUserWritePermission(BasePermission):
+    message = "Editing posts is restricted to the author only."
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
 
 
 class PostList(generics.ListCreateAPIView):
+    # permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    # class ModifyView(APIView):
+    # permission_classes = [IsAuthenticated]  # 仅允许已认证的用户访问
+
+    # def post(self, request):
+    #     # 创建新资源的逻辑
+
+    # def put(self, request, pk):
+    #     # 更新现有资源的逻辑
+
+    # def delete(self, request, pk):
+    #     # 删除现有资源的逻辑
 
 
 class PostDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -21,6 +21,9 @@ urlpatterns = [
     path("api/", include("blog_api.urls", namespace="blog_api")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/user/", include("users.urls", namespace="users")),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+  
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
